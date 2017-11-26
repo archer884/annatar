84d4323a0ccba4684d8570452a5271b24c438713
J/A <archer884@gmail.com>
2017-11-26 17:59:45 -0600

    Rust fmt

--- src/config/options.rs
+++ src/config/options.rs
@@ -73,9 +73,9 @@ impl OptionsBuilder {
         }
 
         let output_format = self.output_format;
-        let output_path = self.output_path.map(|s| s.into()).unwrap_or_else(|| {
-            create_output_file_path(&input_path, output_format)
-        });
+        let output_path = self.output_path
+            .map(|s| s.into())
+            .unwrap_or_else(|| create_output_file_path(&input_path, output_format));
 
         let annotations = if self.annotations.is_empty() {
             return Err(BuildOptionsError {
@@ -138,8 +138,7 @@ fn read_command() -> Result<Options> {
 
     let encoding_group = ArgGroup::with_name("enc_group").args(&["encoding", "jpg", "png"]);
 
-    let app =
-        clap_app!(annatar =>
+    let app = clap_app!(annatar =>
         (version: crate_version!())
         (author: crate_authors!())
         (about: crate_description!())
